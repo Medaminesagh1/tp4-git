375b4085f58107a6497996133c10c47eedba2747
msaghrao <mohamed_amine.saghraoui@etud.univ-jfc.fr>
2023-10-23 17:50:13 +0200

    TP4-Git medamine Saghraoui

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -1,6 +1,9 @@
 package champollion;
 
+
+import java.util.*;
 public class Enseignant extends Personne {
+    private Map<UE, SuperPrevu> enseignements = new HashMap<>();
 
     // TODO : rajouter les autres méthodes présentes dans le diagramme UML
 
@@ -8,7 +11,7 @@ public class Enseignant extends Personne {
         super(nom, email);
     }
 
-    /**
+    /**+
      * Calcule le nombre total d'heures prévues pour cet enseignant en "heures équivalent TD" Pour le calcul : 1 heure
      * de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut 0,75h
      * "équivalent TD"
@@ -17,8 +20,11 @@ public class Enseignant extends Personne {
      *
      */
     public int heuresPrevues() {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        float result = 0f;
+        for (UE ue : enseignements.keySet()) {
+            result += Math.round(heuresPrevuesPourUE(ue));
+        }
+        return Math.round(result);
     }
 
     /**
@@ -31,21 +37,40 @@ public class Enseignant extends Personne {
      *
      */
     public int heuresPrevuesPourUE(UE ue) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        if (enseignements.containsKey(ue)) {
+            SuperPrevu superPrevu = enseignements.get(ue);
+            int heuresCM = superPrevu.getVolumeCM();
+            int heuresTD = superPrevu.getVolumeTD();
+            int heuresTP = superPrevu.getVolumeTP();
+
+            int heuresEquivalentTD = (int)(heuresCM * 1.5f + heuresTD + heuresTP * 0.75f);
+            return heuresEquivalentTD;
+        } else {
+            throw new IllegalArgumentException("L'UE spécifiée n'a pas été trouvée parmi les enseignements de cet enseignant.");
+        }
     }
 
-    /**
-     * Ajoute un enseignement au service prévu pour cet enseignant
-     *
-     * @param ue l'UE concernée
-     * @param volumeCM le volume d'heures de cours magitral
-     * @param volumeTD le volume d'heures de TD
-     * @param volumeTP le volume d'heures de TP
-     */
+
+
     public void ajouteEnseignement(UE ue, int volumeCM, int volumeTD, int volumeTP) {
-        // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        if (volumeCM < 0 || volumeTD < 0 || volumeTP < 0) {
+            throw new IllegalArgumentException("Les volumes d'heures ne peuvent pas être négatifs.");
+        }
+
+        if (!enseignements.containsKey(ue)) {
+            enseignements.put(ue, new SuperPrevu());
+        }
+
+        SuperPrevu superPrevu = enseignements.get(ue);
+        superPrevu.ajouteEnseignement(volumeCM, volumeTD, volumeTP);
     }
+
+
+
+    public boolean estEnSousService() {
+        return heuresPrevues() < 192;
+    }
+
+
 
 }
